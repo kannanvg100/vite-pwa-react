56e10b11363ff6923a94aa5a45923ed401526fc9
kannan <kannan@timber.me>
2025-06-07 00:15:19 +0530

    up10

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import PWABadge from "./PWABadge";
@@ -13,11 +12,8 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React v9</h1>
+      <h1>Vite v10</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
