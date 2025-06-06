80a036c7f3a6e89f13f4f2b6b706b1bc467a1168
kannan <kannan@timber.me>
2025-06-07 00:31:55 +0530

    up15

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite v14</h1>
+      <h1>Vite v15</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
