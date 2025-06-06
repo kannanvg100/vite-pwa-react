4ba2c5d69c8cefbdb06b60d438193a23b22256ef
kannan <kannan@timber.me>
2025-06-06 23:50:19 +0530

    up5

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PWABadge from './PWABadge'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,18 +17,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React v4</h1>
+      <h1>Vite + React v5</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <PWABadge />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
